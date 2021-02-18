b5ffa6896c9c3f9e49a29206c0b908f91ae95efd
itsnotaboutthecell <alpowers@microsoft.com>
2021-02-18 13:11:51 -0600

    Uploaded external tools

---
+++ Assets/ASClearCache.ps1
@@ -0,0 +1,6 @@
+﻿$server = $args[0]
+$databaseId = $args[1]
+
+$clearCache = "<ClearCache xmlns=""http://schemas.microsoft.com/analysisservices/2003/engine""><Object><DatabaseID>$($databaseId)</DatabaseID></Object></ClearCache>"
+
+Invoke-AsCmd -Server:$server -Database:$databaseId  -Query:$clearCache
